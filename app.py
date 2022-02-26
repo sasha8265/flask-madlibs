5b7fa6c9ca1e401542d9c6d7d1b6a375f70a3b15
Sasha Czerniawski <sashacandoit@gmail.com>
2022-02-26 11:28:56 -0500

    everything works

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+from stories import story
 
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = "sashacandoit"
+
 
 @app.route('/')
 def form_home():
-    return render_template("form.html")
+    """Generate form page and populate input labels with prompts"""
+    prompts = story.prompts
+    return render_template("form.html", prompts=prompts)
 
 
 @app.route('/story')
 def show_story():
 
+    # all_args = dict(request.args)
+    # answers = jsonify(all_args)
 
+    text = story.generate(request.args)
 
-    return render_template("story.html")
+    return render_template("story.html", text=text)
 
